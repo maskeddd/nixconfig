670782eb2503fefb7d602f6e0b651429d61357f6
masked <37945842+maskeddd@users.noreply.github.com>
2025-12-18 14:28:49 +1000

    sign commits

--- modules/home/common/git.nix
+++ modules/home/common/git.nix
@@ -1,21 +1,31 @@
 {
   config,
+  pkgs,
+  lib,
   ...
 }:
+
+let
+  signerBinary =
+    if pkgs.stdenv.isDarwin then
+      "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+    else
+      "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+in
 {
-  programs = {
-    git = {
-      enable = true;
-      settings = {
-        user = {
-          email = config.me.email;
-          name = config.me.fullname;
-        };
-      };
-      ignores = [
-        "*~"
-        "*.swp"
-      ];
+  programs.git = {
+    enable = true;
+
+    settings.user = {
+      email = config.me.email;
+      name = config.me.fullname;
+    };
+
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBr31av52kwxC8FIIeSKosnY6NeYnrR0zQj8Ziqfqtqj";
+      format = "ssh";
+      signer = signerBinary;
+      signByDefault = true;
     };
   };
 }
