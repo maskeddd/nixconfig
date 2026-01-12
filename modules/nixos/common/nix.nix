51937c64960845174c8179e44d0d3b43703b7c41
masked <37945842+maskeddd@users.noreply.github.com>
2026-01-12 15:09:58 +1000

    darwin changes

---
+++ modules/nixos/common/nix.nix
@@ -0,0 +1,33 @@
+{
+  flake,
+  lib,
+  ...
+}:
+
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+    overlays = lib.attrValues self.overlays;
+  };
+
+  nix = {
+    settings = {
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://cache.garnix.io"
+        "https://zed.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+      ];
+    };
+  };
+}
--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -5,22 +5,8 @@
         "nix-command"
         "flakes"
       ];
-
-      substituters = [
-        "https://hyprland.cachix.org"
-        "https://cache.garnix.io"
-        "https://zed.cachix.org"
-
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
-      ];
     };
   };
-
-  nixpkgs.config.allowUnfree = true;
 
   networking.networkmanager.enable = true;
 
