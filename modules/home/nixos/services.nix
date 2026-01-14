30cff41a7cf551d75a2cc5b622ab2d6c30cf1591
masked <37945842+maskeddd@users.noreply.github.com>
2026-01-14 15:31:33 +1000

    feat: declarative flatpaks

---
+++ modules/home/nixos/services.nix
@@ -0,0 +1,25 @@
+{ flake, pkgs, ... }:
+let
+  inherit (flake) inputs;
+in
+{
+  imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
+
+  services.flatpak = {
+    packages = [
+      "org.vinegarhq.Sober"
+      "org.vinegarhq.Vinegar"
+      rec {
+        appId = "com.hypixel.HytaleLauncher";
+        sha256 = "1v153k3vns64axybkd08r63jrcj8csqks5777bncyw1rpn6rflpn";
+        bundle = "${pkgs.fetchurl {
+          url = "https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak";
+          inherit sha256;
+        }}";
+      }
+    ];
+    update.onActivation = true;
+    uninstallUnmanaged = true;
+    uninstallUnused = true;
+  };
+}
