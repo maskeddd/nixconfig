1bdc7b46bf158f2885286c611dabc1723db1c11d
masked <37945842+maskeddd@users.noreply.github.com>
2026-03-04 18:38:57 +1000

    use stylix for spotify

--- modules/home/common/stylix.nix
+++ modules/home/common/stylix.nix
@@ -2,7 +2,7 @@
   stylix = {
     targets = {
       zed.enable = false;
-      spicetify.enable = false;
+      spicetify.enable = true;
       nixcord.enable = false;
       helix.enable = false;
       opencode.enable = false;
