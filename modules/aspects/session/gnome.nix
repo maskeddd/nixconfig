5c1bc1697354d94785387c520af9d4fdd5c69eff
masked <37945842+maskeddd@users.noreply.github.com>
2026-05-09 02:41:45 +1000

    gnome image/audio mime defaults

--- modules/aspects/session/gnome.nix
+++ modules/aspects/session/gnome.nix
@@ -23,7 +23,33 @@
           simple-scan
           gnome-music
           gnome-text-editor
+          gnome-console
         ];
+      };
+
+    hmLinux =
+      { pkgs, lib, ... }:
+      let
+        extractMimeTypes =
+          desktopFile:
+          let
+            line = lib.findFirst (lib.hasPrefix "MimeType=") "" (
+              lib.splitString "\n" (builtins.readFile desktopFile)
+            );
+          in
+          lib.filter (s: s != "") (lib.splitString ";" (lib.removePrefix "MimeType=" line));
+      in
+      {
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications =
+            lib.genAttrs (extractMimeTypes "${pkgs.loupe}/share/applications/org.gnome.Loupe.desktop") (
+              _: "org.gnome.Loupe.desktop"
+            )
+            // lib.genAttrs
+              (extractMimeTypes "${pkgs.decibels}/share/applications/org.gnome.Decibels.desktop")
+              (_: "org.gnome.Decibels.desktop");
+        };
       };
   };
 }
