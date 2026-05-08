a239d5c9994ac998416abf50f14c7b20bca36588
masked <37945842+maskeddd@users.noreply.github.com>
2026-05-09 02:41:43 +1000

    spotify uri redirect

--- modules/aspects/apps/spotify.nix
+++ modules/aspects/apps/spotify.nix
@@ -1,9 +1,41 @@
 { inputs, ... }:
 {
   flake-file.inputs.spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+  den.aspects.spotify = {
+    homeManager = {
+      imports = [ inputs.spicetify-nix.homeManagerModules.spicetify ];
+      programs.spicetify.enable = true;
+    };
 
-  den.aspects.spotify.homeManager = {
-    imports = [ inputs.spicetify-nix.homeManagerModules.spicetify ];
-    programs.spicetify.enable = true;
+    hmLinux =
+      { pkgs, ... }:
+      {
+        home.packages = [
+          (pkgs.writeShellScriptBin "spotify-redirect" ''
+            if [[ "$1" =~ open\.spotify\.com/(track|album|playlist|artist|episode|show)/([a-zA-Z0-9]+) ]]; then
+              exec ${pkgs.spotify}/bin/spotify --uri="spotify:''${BASH_REMATCH[1]}:''${BASH_REMATCH[2]}"
+            fi
+            exec ${pkgs.brave}/bin/brave "$1"
+          '')
+        ];
+
+        xdg.desktopEntries.spotify-redirect = {
+          name = "Spotify Redirect";
+          exec = "spotify-redirect %u";
+          mimeType = [
+            "x-scheme-handler/http"
+            "x-scheme-handler/https"
+          ];
+          noDisplay = true;
+        };
+
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "x-scheme-handler/http" = "spotify-redirect.desktop";
+            "x-scheme-handler/https" = "spotify-redirect.desktop";
+          };
+        };
+      };
   };
 }
